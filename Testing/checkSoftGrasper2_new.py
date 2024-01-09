d96daccc882ed84d888f3f01c993b48f2e174a12
quantumRAV <46093108+quantumRAV@users.noreply.github.com>
2024-01-09 14:34:57 -0500

    Branch for testing picking up deformable objects.
    This branch will have a script for picking up a deformable object.

--- Testing/checkSoftGrasper2_new.py
+++ Testing/checkSoftGrasper2_new.py
@@ -8,9 +8,11 @@ SG  = SoftGrasper(COM_Port = 'COM7',BaudRate=460800,timeout=1,controllerProfile=
 while (True):
 
     # construct port commands and send to Teensy
+    SG.PressurePorts[3].portStatus = PortActions.HOLD
+    SG.PressurePorts[2].portStatus = PortActions.HOLD
     SG.PressurePorts[1].portStatus = PortActions.HOLD
     SG.PressurePorts[0].portStatus = PortActions.INFLATE_AND_MODULATE
-    SG.PressurePorts[0].commandedPressure = 10.3
+    SG.PressurePorts[0].commandedPressure = 2
 
     byteList = SG.ConstructPortCommand()
     numBytes = SG.sendCommunicationArray(byteList=byteList)
@@ -19,6 +21,9 @@ while (True):
     # read serial data
     SG.readSerialData()
     SG.getJawChangePressureVals()
+    if len(SG.PressureArray[0]) >= 1:
+        print("Closer, Jaw 1, Jaw 2, Jaw 3:%f,%f,%f,%f"%(SG.PressureArray[0][-1],SG.PressureArray[1][-1],
+                                                         SG.PressureArray[2][-1],SG.PressureArray[3][-1]))
     print('New')
 
     #print("BigLoop:"+str(vv))

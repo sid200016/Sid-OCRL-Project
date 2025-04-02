28c399612dee71b47fe1ddcded9ea916dc99af29
Siddarth Ashok <siddashok@gmail.com>
2025-04-02 01:11:05 -0400

    Got the robot to visualize through opengl

---
+++ Project_CPP/build/CMakeFiles/my_sim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_sim.dir/main.cpp.o"
+  "CMakeFiles/my_sim.dir/main.cpp.o.d"
+  "my_sim"
+  "my_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

ce1be9a95fe2411ba04db1ed271911f2954412d5
Siddarth Ashok <siddashok@gmail.com>
2025-04-03 23:31:47 -0400

    added RK4 integrator templatized + helper function for vector operations need to make small edits to make helpers templatized

--- Project_CPP/main.cpp
+++ Project_CPP/main.cpp
@@ -32,35 +32,96 @@
 #include "math/tiny/tiny_algebra.hpp"
 #include "math/tiny/tiny_dual.h"
 #include "math/tiny/tiny_dual_double_utils.h"
-#include "math/tiny/tiny_dual_utils.h"
+#include "dynamics/forward_dynamics.hpp"
+#include "dynamics/integrator.hpp"
+
+//#include "math/tiny/tiny_dual_utils.h"
 using namespace tds;
 using namespace TINY;
 
 #ifdef USE_TINY
 
-typedef double TinyDualScalar;
-typedef double MyScalar;
-typedef ::TINY::DoubleUtils MyTinyConstants;
-typedef TinyVector3<double, DoubleUtils> Vector3;
-typedef TinyQuaternion<double, DoubleUtils> Quaternion;
-typedef TinyAlgebra<double, MyTinyConstants> MyAlgebra;
+//typedef double TinyDualScalar;
+//typedef double MyScalar;
+//typedef ::TINY::DoubleUtils MyTinyConstants;
+//typedef TinyVector3<double, DoubleUtils> Vector3;
+//typedef TinyQuaternion<double, DoubleUtils> Quaternion;
+//typedef TinyAlgebra<double, MyTinyConstants> MyAlgebra;
 
-#else
-#include "math/eigen_algebra.hpp"
-typedef EigenAlgebra MyAlgebra;
-typedef typename MyAlgebra::Scalar MyScalar;
-typedef typename MyAlgebra::Vector3 Vector3;
-typedef typename MyAlgebra::Quaternion Quaternion;
-typedef typename MyAlgebra::VectorX VectorX;
-typedef typename MyAlgebra::Matrix3 Matrix3;
-typedef typename MyAlgebra::Matrix3X Matrix3X;
-typedef typename MyAlgebra::MatrixX MatrixX;
+//#else
+//#include "math/eigen_algebra.hpp"
+//typedef EigenAlgebra MyAlgebra;
+//typedef typename MyAlgebra::Scalar MyScalar;
+//typedef typename MyAlgebra::Vector3 Vector3;
+//typedef typename MyAlgebra::Quaternion Quaternion;
+//typedef typename MyAlgebra::VectorX VectorX;
+//typedef typename MyAlgebra::Matrix3 Matrix3;
+//typedef typename MyAlgebra::Matrix3X Matrix3X;
+//typedef typename MyAlgebra::MatrixX MatrixX;
 #endif
+typedef TinyDual<double> TinyDualScalar;
+typedef TinyAlgebra<TinyDualScalar, TinyDualDoubleUtils> MyAlgebra;
 //std::string urdf_path = "../robot_arm_3/urdf/robot_arm_3.urdf";
+MyAlgebra::Vector3 gravity = MyAlgebra::Vector3(MyAlgebra::from_double(0.),
+                                                 MyAlgebra::from_double(0.),
+                                                 MyAlgebra::from_double(-9.81));
+double dt = 0.01;
+//Multiply Vector by a certain value val, elementwise
+std::vector<double> multVec(std::vector<double> v1, double val)
+{
+  std::vector<double> newVec(v1.size());
+  std::transform(v1.begin(), v1.end(), newVec.begin(), [val](double x){return x*val;});
+  return newVec;
+}
+//Divide Vector by a certain value val, elementwise
+std::vector<double> divVec(std::vector<double> v1, double val)
+{
+  std::vector<double> newVec(v1.size());
+  std::transform(v1.begin(), v1.end(), newVec.begin(), [val](double x){return x/val;});
+  return newVec;
+}
+//add 2 vectors element wise
+std::vector<double> addVec(std::vector<double> v1, std::vector<double> v2)
+{
+  std::vector<double> newVec(v1.size());
+  std::transform(v1.begin(), v1.end(), v2.begin(), newVec.begin(), [](double a,double b) {return a+b;});
+  return newVec;
+}
+
+//Get state from mb (stacked q + dq)
+//Template Algebra to easily switch from data types: Double, eigen, tinydual, etc
 template <typename Algebra>
+std::vector<double> get_state(tds::MultiBody<Algebra>* mb) {
+  std::vector<double> state;
+  for (int i = 0; i < mb->dof(); i++) {
+    state.push_back(Algebra::to_double(mb->q(i)));
+  }
+  for (int i = 0; i < mb->dof(); i++) {
+    state.push_back(Algebra::to_double(mb->qd(i)));
+  }
+  return state;
+}
+//set mb state(q+dq)
+//Template Algebra to easily switch from data types: Double, eigen, tinydual, etc
+template <typename Algebra>
+void set_state(tds::MultiBody<Algebra>* mb, const std::vector<double>& state)
+{
+  for (int i = 0; i < 2*mb->dof(); i++)
+  {
+    if (i < mb->dof())
+    {
+      mb->q(i) = Algebra::from_double(state[i]);
+    }
+    else
+    {
+      mb->dq(i) = Algebra::from_double(state[i]);
+    }
+  }
+}
 
-
-//Helper function for initial setup of the 
+//Helper function for initial setup of the visulaizer
+//Template Algebra to easily switch from data types: Double, eigen, tinydual, etc
+template <typename Algebra>
 tds::UrdfStructures<Algebra> setup_robot(OpenGLUrdfVisualizer<Algebra>& visualizer,
   tds::UrdfCache<Algebra>& cache,
   const std::string& urdf_path,
@@ -69,11 +130,11 @@ tds::UrdfStructures<Algebra> setup_robot(OpenGLUrdfVisualizer<Algebra>& visualiz
     
  
 
-  mb->base_X_world().translation = typename Algebra::Vector3(0, 0, 0);
-  mb->base_X_world().rotation = Algebra::Matrix3::Identity();
+  mb->base_X_world().translation = typename Algebra::Vector3(Algebra::from_double(0), Algebra::from_double(0), Algebra::from_double(0));
+  mb->base_X_world().rotation = Algebra::Matrix3::get_identity();
 
   for (int i = 0; i < mb->dof(); ++i) {
-    mb->q(i) = 0.0;
+    mb->q(i) = Algebra::from_double(0.0);
   }
 
   mb->initialize();
@@ -96,7 +157,7 @@ std::cout<<"links"<<std::endl;
 std::cout << urdf_struct.links.size()<<std::endl;
 std::cout<<"dofs"<<std::endl;
 std::cout<<mb->dof()<<std::endl;
-std::cout<<link_names<<std::endl;
+std::cout<<"link_names"<<std::endl;
 for (int j = 0; j < urdf_struct.links.size();j++)
 {
     std::cout << "Link " << j << ": " << urdf_struct.links[j].link_name << std::endl;
@@ -127,18 +188,91 @@ for (int j = 0; j < urdf_struct.links.size();j++)
   visualizer.render();
   return urdf_struct;
   }
+  //Update render with required joint angle. Call FK and resync, otherwise all urdfs will just go to origin
+  //Template Algebra to easily switch from data types: Double, eigen, tinydual, etc
   template <typename Algebra>
   void update_render(OpenGLUrdfVisualizer<Algebra>& visualizer,
     tds::MultiBody<Algebra>* mb,  std::vector<double>& joint_angles){
       
 
-      for (int i = 0; i < mb->dof(); ++i) {
-      mb->q(i) = joint_angles[i];
+      for (int i = 0; i < mb->dof(); i++) {
+      mb->q(i) = Algebra::from_double(joint_angles[i]);
       }
       forward_kinematics(*mb);
       visualizer.sync_visual_transforms(mb);
       visualizer.render();
     
+  }
+  //RK4 integrator
+  //TDS provides semi-implicit euler, so this is extra
+  //Setup in a way that it can easily be ported over to the intergrator.h header file
+  //Template Algebra to easily switch from data types: Double, eigen, tinydual, etc
+  template <typename Algebra>
+  std::vector<double> RK4_Integrator(tds::MultiBody<Algebra>* mb, bool rbdl, World<Algebra>& world){
+  std::vector<double> state_origin = get_state(mb);
+  //k1
+  tds::forward_dynamics(*mb, world.get_gravity(), rbdl);
+  std::vector<double>k1;
+  for (int i = 0; i < 2*mb->dof(); i++) {
+    if (i < mb->dof()){
+      k1.push_back(MyAlgebra::to_double(mb->dq(i))*dt);
+    }
+    else{
+      k1.push_back(MyAlgebra::to_double(mb->ddq(i-mb->dof()))*dt);
+    }
+  }
+  //k2
+  std::vector<double> new_x = addVec(state_origin, divVec(k1, 2));
+  set_state(mb, new_x);
+  std::vector<double>k2;
+  tds::forward_dynamics(*mb, world.get_gravity(), rbdl);
+  for (int i = 0; i < 2*mb->dof(); i++) {
+    if (i < mb->dof()){
+      k2.push_back(MyAlgebra::to_double(mb->dq(i))*dt);
+    }
+    else{
+      k2.push_back(MyAlgebra::to_double(mb->ddq(i-mb->dof()))*dt);
+    }
+  }
+  //k3
+  std::vector<double> new_x2 = addVec(state_origin, divVec(k2, 2));
+  set_state(mb, new_x2);
+  std::vector<double>k3;
+  tds::forward_dynamics(*mb, world.get_gravity(), rbdl);
+  for (int i = 0; i < 2*mb->dof(); i++) {
+    if (i < mb->dof()){
+      k3.push_back(MyAlgebra::to_double(mb->dq(i))*dt);
+    }
+    else{
+      k3.push_back(MyAlgebra::to_double(mb->ddq(i-mb->dof()))*dt);
+    }
+  }
+  //k4
+  std::vector<double> new_x3 = addVec(state_origin, k3);
+  set_state(mb, new_x3);
+  std::vector<double>k4;
+  tds::forward_dynamics(*mb, world.get_gravity(), rbdl);
+  for (int i = 0; i < 2*mb->dof(); i++) {
+    if (i < mb->dof()){
+      k4.push_back(MyAlgebra::to_double(mb->dq(i))*dt);
+    }
+    else{
+      k4.push_back(MyAlgebra::to_double(mb->ddq(i-mb->dof()))*dt);
+    }
+  }
+  final_state = addVec(state_origin, 
+                       divVec(
+                        addVec(k1, 
+                          addVec(
+                            multVec(k2, 2), 
+                            addVec(
+                              multVec(k3, 2), k4)
+                            )
+                          )
+                        ), 
+                        6.0);
+  set_state(mb, state_origin);
+  return final_state;
   }
   //Main Loop
 int main(int argc, char* argv[]) {
@@ -154,7 +288,7 @@ int main(int argc, char* argv[]) {
     
     
     update_render(visualizer, mb,joint_angles);
-    sleep(0.01);
+    sleep(0.01);  
 
   
 }
